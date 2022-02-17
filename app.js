e7a3cb6693d3cc102b58a2e79be83d930ed75bad
Smyllet <smyllet.gamer@gmail.com>
2022-02-17 12:33:59 +0100

    [ADD] Implement websocket

--- app.js
+++ app.js
@@ -1,28 +1,167 @@
-const http = require('http')
 const config = require('./config.json')
-const EDTManager = require("./class/EDTManager");
+const Logs = require("./class/Logs");
+const {webkit} = require("playwright");
+const cheerio = require("cheerio");
 
 let cache = {
     expireDate: new Date(),
     data: []
 }
 
-const requestListener = async function (req, res) {
-    res.writeHead(200, {"Content-Type": "application/json"});
+/**
+ * @type {[{username: string, password: string}]}
+ */
+let cacheCredentials = []
 
-    if(cache.expireDate > new Date()) {
-        res.end(JSON.stringify(cache.data))
-    } else {
-        let json = await EDTManager.scrapAllEDT(new Date())
-        cache.data = json
-        cache.expireDate = new Date((new Date().getTime()/1000 + 3600)*1000)
-        res.end(JSON.stringify(json))
+const server = require('http').createServer()
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "http://localhost:4200",
+        methods: ["GET", "POST"]
     }
-}
-
-const server = http.createServer(requestListener);
-server.listen(config.http.port, config.http.host, () => {
-    console.log(`Server is running on http://${config.http.host}:${config.http.port}`);
 })
 
+io.on('connection', (socket) => {
+    console.log(`Connecté au client ${socket.id}`)
+    socket.on('login', async (/** {username: string, password: string} */ credentials) => {
+        try {
+            // Test du cache identifiant + edt
+            if(cache.expireDate > new Date() && cacheCredentials.find(c => c === credentials)) {
+                Logs.info(`Connexion cache de ${credentials.username}`)
+                socket.emit('login', true)
+                return socket.emit('updateEdt', cache.data)
+            }
+            Logs.info(`Connexion à l'ENT de ${credentials.username}`)
 
+            // Lancement du navigateur
+            const browser = await webkit.launch()
+            const context = await browser.newContext()
+            const page = await context.newPage()
+
+            // Ouverture de l'ent
+            await page.goto(`https://cas.iut.univ-paris8.fr/login?service=https%3a%2f%2fent.iut.univ-paris8.fr%2f`)
+
+            // Connexion
+            await page.fill("#username", credentials.username)
+            await page.fill("#password", credentials.password)
+            await page.click("text=SE CONNECTER")
+
+            // Test de succès de la connexion
+            if(!(await page.title()).includes('ENT')) {
+                return socket.emit('login', false)
+            } else {
+                socket.emit('login', true)
+                cacheCredentials.push(credentials)
+            }
+
+            if(cache.expireDate > new Date()) {
+                Logs.info('Récupération EDT cache')
+                return socket.emit('updateEdt', cache.data)
+            } else Logs.info('Récupération EDT scrapping')
+
+            // récupération Emploi du temps
+            await page.goto(`https://ent.iut.univ-paris8.fr/edt/presentations.php`)
+
+            /*await page.click(".ui-selectmenu-button")
+            await page.screenshot({path: "lastCalendarView.png"})
+            await page.click(`#selectpromo-menu>li:has-text("BUT INFO S2")`)*/
+
+            // Récupération de la liste des semaines
+            await page.click("#selectsem-button")
+            let availableDate = cheerio.load(await page.content())("#selectsem-menu").html().split(">").filter(el => el.includes("</li")).map(el => el.replace('</li', ''))
+
+            let days = []
+
+            // Pour chaque semaines disponible
+            for(const stringDate of availableDate) {
+                let splitDate = stringDate.split('/')
+                let dayDate = splitDate[0]
+                let monthDate = splitDate[1]
+                let yearDate = splitDate[2]
+                let mondayDate = new Date(yearDate, monthDate-1, dayDate, 0, 0)
+
+                console.log(mondayDate.toLocaleDateString())
+
+                await page.click(`#selectsem-menu>li:has-text("${stringDate}")`)
+                await page.click("#selectsem-button")
+
+                let html = await page.content()
+
+                const $ = cheerio.load(html)
+
+                let edtHtml = $("#quadrillage").html()
+                let profList = $("#selectprof").find('option').toArray().map(el => el.children[0].data)
+
+                let edtDays = []
+
+                if(edtHtml.length > 0) {
+                    edtDays = edtHtml.split("</div><div").map(el => {
+                        if(!el.startsWith("<div")) el = "<div" + el
+                        if(!el.endsWith("</div>")) el = el + "</div>"
+                        return el
+                    }).filter(el => {
+                        return !el.includes("plageDIVn");
+                    }).map(el => {
+                        try {
+                            let day = {}
+
+                            try {
+                                day.mat = el.split("<strong>")[1].split("</strong>")[0]
+                            } catch (e) {
+                                try {
+                                    day.mat = el.split('<span class="plageCTRL">')[1].split("</span>")[0]
+                                } catch (e) {
+                                    day.mat = "Matière inconnu"
+                                }
+                            }
+
+
+                            try {
+                                let profEtSalle = el.split(`<span class="plageHG">`)
+                                let profLettre = profEtSalle[1].split("</span>")[0]
+                                let profName = profList.find(prof => prof.includes(`[${profLettre}]`))
+                                day.prof = (profName) ? profName.toUpperCase() : (profEtSalle.length < 3) ? "Autonomie" : profLettre
+                                day.salle = profEtSalle[profEtSalle.length-1].split("</span>")[0].split("&")[0]
+                            } catch (e) {
+                                day.prof = "Prof inconnu"
+                                day.salle = "Salle inconnu"
+                            }
+
+
+                            let style = el.split("style=\"")[1].split("\"")[0].split(";")
+
+                            day.debut = mondayDate.getTime()/1000
+                            day.debut += 24 * 60 * 60 * style.find(s => s.includes("margin-left")).split(':')[1].split("%")[0]/80*4
+                            day.debut += 60 * (style.find(s => s.includes("top")).split(':')[1].split("px")[0]-30+480)
+
+                            day.debutText = (new Date(day.debut * 1000)).toLocaleString()
+
+                            day.fin = day.debut
+                            day.fin += 60 * (style.find(s => s.includes("height")).split(':')[1].split("px")[0])
+
+                            day.finText = (new Date(day.fin * 1000)).toLocaleString()
+
+                            return day
+                        } catch (e) {
+                            return null
+                        }
+                    })
+                }
+
+                days = days.concat(edtDays)
+                socket.emit('updateEdt', days)
+                cache = {
+                    expireDate: new Date((new Date().getTime()/1000 + 3600)*1000),
+                    data: days
+                }
+            }
+        } catch (e) {
+            Logs.error(e)
+            socket.emit('error')
+        }
+    })
+})
+
+server.listen(config.http.port, () => {
+    console.log(`Server is running on http://${config.http.host}:${config.http.port}`)
+})
